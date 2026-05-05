5c97d06e2c56d959d90853deb9886d006c2f22f9
TJustice785 <tsepangtsehla31@gmail.com>
2026-05-05 13:49:18 +0200

    deploy: increase chunk size limit further to 2000kb

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     build: {
-      chunkSizeWarningLimit: 1000,
+      chunkSizeWarningLimit: 2000,
     },
   },
 });
